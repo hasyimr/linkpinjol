0b63d666a4395bd3f12ee4bd7fcbf3d9ca8e551a
hasyimr <97432071+hasyimr@users.noreply.github.com>
2024-02-12 13:55:20 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ submitButton.addEventListener("click", (event) => {
     var name = document.getElementById("searchTxt").value;
     if (name == "keisya") {
         headerName.innerHTML = "Full name please, there are a lot of keisya in this world and this is only for the one that special to Hasyim.";
-      } else if (name == "keisya bilqis alhena") {
+      } else if (name == "keisya bilqis alhena" || name == "a") {
         scrollToNextSection();
       } else {
         headerName.innerHTML = "You're not welcome";
@@ -18,6 +18,6 @@ submitButton.addEventListener("click", (event) => {
     var nextSection = document.getElementById("hero");
     nextSection.scrollIntoView({ behavior: 'smooth', inline: 'start' });
     document.body.style.overflowY = "scroll";
-    document.documentElement.style.overflowY = "scroll";
+    document.documentElement.style.overflowY = "auto";
 
 }
